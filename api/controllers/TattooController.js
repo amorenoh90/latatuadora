e81645a40e6ea221eb1e53483146d189c223e1eb
enrique199413@gmail.com <enrique199413@gmail.com>
2017-05-10 16:01:13 -0500

    #4 add the add method to create a tattoo

--- api/controllers/TattooController.js
+++ api/controllers/TattooController.js
@@ -6,6 +6,25 @@
  */
 
 module.exports = {
-
+  add: function (req, res) {
+    Tattoo.create({tatuaje: 1, bodyPart: 'Pie', element:'Tierra', dimensionsX:12.5, dimensionsY:10.5}).exec(function (err, records) {
+      if (err) {
+        res.send(500, 'Error');
+      } else {
+        console.log(records, 'enriquelc-----');
+        res.send(200, 'nice');
+      }
+    });
+  },
+  allTattoos: function (req, res) {
+    Tattoo.find()
+      .populate('tatuaje')
+      .exec(function(err, tattoos) {
+        if(err) {
+          res.send('error');
+        }
+        res.send(tattoos);
+      });
+  }
 };
 
