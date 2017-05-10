c3dd15289a6b299c12179fd3c7b04d41003d4297
edwinvarroyo <e28v11a96@gmail.com>
2017-05-10 15:26:41 -0500

    #4 add model Tattoo

---
+++ api/models/Tattoo.js
@@ -0,0 +1,47 @@
+/**
+ * Tattoo.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  identity: 'Tattoo',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      autoIncrement: true,
+      unique: true
+    },
+    element: {
+      type: 'string',
+      size: 40
+    },
+    style: {
+      type: 'string',
+      size: 40
+    },
+    bodyPart:{
+      type: 'string',
+      size: 40
+    },
+    dimensionsX: {
+      type: 'float',
+      size: 5
+    },
+    dimensionsY: {
+      type: 'float',
+      size: 5
+    },
+    image: {
+      type: 'string',
+      size: 40
+    },
+    typeTattoos: {
+      collection: "TypeTattoo",
+      via: "typetattoo_id"
+    }
+  }
+};
+
