530255b2753373bde05ea384a8c2e537b8228e5d
edwinvarroyo <e28v11a96@gmail.com>
2017-05-12 19:18:20 -0500

    #4 add route post for add new Tattoo

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
 
   /***************************************************************************
   *                                                                          *
@@ -45,5 +45,12 @@ module.exports.routes = {
   * for configuration options and examples.                                  *
   *                                                                          *
   ***************************************************************************/
+
+
+  //>Routes Tattoo
+
+    'post /tattoo/add': 'TattooController.add'
+
+  //<Routes Tattoo
 
 };
