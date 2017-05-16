4f2dbd97e002c1cf9541a2418a0bf79d6abdf1e5
edwinvarroyo <e28v11a96@gmail.com>
2017-05-15 21:33:26 -0500

    #4 create Tattoo Test

--- test/integration/controllers/TattooController.test.js
+++ test/integration/controllers/TattooController.test.js
@@ -4,22 +4,33 @@
 var request = require('supertest');
 
 describe('TattooController', function() {
-    describe('CRUD tattoo', function () {
+  describe('CRUD tattoo', function () {
+    var tattos,
+      mockTattoo = {
+      name: "tattoo",
+      tattootype: 1,
+      partbody: 1,
+      element: 1,
+      dimensionsX: 12.5,
+      dimensionsY: 10.5,
+      publicate: false,
+      image: "kjabfiabf.jpg"
+    }
     it('should to add a new tattoo with params', function (done) {
-      var mockTattoo = {
-        typetattoo: 1,
-        partbody: 1,
-        element:1,
-        dimensionsX:12.5,
-        dimensionsY:10.5,
-        publicate: false
-      }
       request(sails.hooks.http.app)
-        .post('/tattoo/add')
+        .post('/tattoo/add') //create
         .send(mockTattoo)
         .expect(200)
-        .expect('nice', done);
-      Tattoo.findOne(mockTattoo)
+        .expect('nice', done)
+    });
+    it('should get tattoo', function (done) {
+
+      Tattoo.find(mockTattoo, function (err, res) {
+        sails.log(":v",res, err);
+        // res.length.should.not.be.eql(0);
+        res.to.have.property('id').to.not.equal('null')
+        done();
+      });
     });
   });
 
