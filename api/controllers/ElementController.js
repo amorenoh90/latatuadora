2e510c9f395416ea088916497f33f7ed7e7e3e87
edwinvarroyo <e28v11a96@gmail.com>
2017-05-15 21:30:38 -0500

    #4 add function for check database

--- api/controllers/ElementController.js
+++ api/controllers/ElementController.js
@@ -7,12 +7,19 @@
 
 module.exports = {
   add: function (req, res) {
-    Element.create({element: "agua"}).exec(function (err, records) {
+    var newelement= {
+      element: req.body.element
+    }
+    Element.create(newelement).exec(function (err, records) {
       if (err) {
         res.send(500, 'Error');
-      } else {
-        console.log(records, 'enriquelc-----');
-        res.send(200, 'nice');
+      }
+      else { // For check new Element in DB
+         Element.findOne(newelement).exec(function (err, record) {
+          if (err) res.send(500, 'Error');
+          if (!record) res.send(404, "Could not find Element")
+          else res.send(200, 'nice');
+         });
       }
     });
   },
