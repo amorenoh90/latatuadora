7575d3e970f1dc0dcdd3104e2af2cc2417e2270a
edwinvarroyo <e28v11a96@gmail.com>
2017-05-17 19:30:08 -0500

    #4 add test for CRUD functions from Element Controller

--- test/integration/controllers/ElementController.test.js
+++ test/integration/controllers/ElementController.test.js
@@ -1,19 +1,69 @@
 /**
  * Created by edwin on 15/05/17.
  */
-
-var request = require('supertest');
+var request = require('supertest'),
+  assert = require('assert');
 
 describe('ElementController', function() {
 
-  describe('CRUD Element', function () {
-    it('should to add a new Element', function (done) {
+  describe('CRUD Body Element ', function () {
+  	var mockElementInsert = {
+  		element: "Agua"
+  	},
+  		mockElementUpdate = {
+  			element: "Tierra",
+  			id: 2
+  		}
+    it('should to add a new Element ', function (done) {
+    	Element.create({element:'Fuego'}).exec(function (err, res, element){});
+
       request(sails.hooks.http.app)
         .post('/element/add')
-        .send({ element: 'flor' })
-        .expect(200)
-        .expect('nice', done);
+        .send(mockElementInsert)
+        .expect(200, done);
     });
+    it('should get all Elements', function (done) {
+      request(sails.hooks.http.app)
+        .get('/element/get')
+        .set('Accept', 'application/json')
+        .expect(function(res) {
+          assert.equal(res.body.length, 2)
+          assert.notEqual(res.body[0].hasOwnProperty('id'), null);
+          assert.equal(res.body[1].element, mockElementInsert.element);
+          
+        })
+        .expect(200, done);
+    });
+    it('should update a Element', function (done) {
+      request(sails.hooks.http.app)
+        .post('/element/update')
+        .send(mockElementUpdate)
+        .expect(function(res){})
+        .expect(200, done);
+    });
+    it('should get a Element Updated', function (done) {
+      request(sails.hooks.http.app)
+        .post('/element/getOne')
+        .send({id: mockElementUpdate.id})
+        .expect(function(res) {
+          assert.equal(res.body.id, mockElementUpdate.id);
+          assert.equal(res.body.element, mockElementUpdate.element);        })
+        .expect(200, done);
+    });
+    it('should delete a Element', function (done) {
+      request(sails.hooks.http.app)
+      .delete('/element/delete')
+      .send({id: mockElementUpdate.id})
+      .expect(200, done);
+    }); 
+    it('Should get a []', function (done) {
+      request(sails.hooks.http.app)
+        .post('/element/getOne')
+        .send({id: mockElementUpdate.id})
+        .expect([])
+        .expect(200, done);
+       //Element.destroy({element:'Pierna'}).exec(function (err, res, element){});
+    }); 
   });
 
 });
