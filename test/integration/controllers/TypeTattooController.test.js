21c8455d21758959edb47cdff42ed7ca07775d44
edwinvarroyo <e28v11a96@gmail.com>
2017-05-17 19:30:42 -0500

    #4 add test for CRUD functions from TypeTattoo Controller

--- test/integration/controllers/TypeTattooController.test.js
+++ test/integration/controllers/TypeTattooController.test.js
@@ -2,17 +2,69 @@
  * Created by edwin on 15/05/17.
  */
 
-var request = require('supertest');
+var request = require('supertest'),
+  assert = require('assert');
 
-describe('TypeTatooController', function() {
+describe('TypeTattooController', function() {
 
-  describe('CRUD TypeTatoo', function () {
-    it('should to add a new TypeTatoo', function (done) {
+  describe('CRUD Body TypeTattoo ', function () {
+  	var mockTypeTattooInsert = {
+  		type: "Aqua"
+  	},
+  		mockTypeTattooUpdate = {
+  			type: "milenial",
+  			id: 3
+  		}
+    it('should to add a new TypeTattoo ', function (done) {
+    	TypeTattoo.create({type:'Fuego'}).exec(function (err, res, type){});
+
       request(sails.hooks.http.app)
         .post('/typetattoo/add')
-        .send({ typetatto: 'chido' })
-        .expect(200)
-        .expect('nice', done);
+        .send(mockTypeTattooInsert)
+        .expect(200, done);
     });
+    it('should get all TypeTattoos', function (done) {
+      request(sails.hooks.http.app)
+        .get('/typetattoo/get')
+        .set('Accept', 'application/json')
+        .expect(function(res) {
+          assert.equal(res.body.length, 2)
+          assert.notEqual(res.body[0].hasOwnProperty('id'), null);
+          assert.equal(res.body[1].type, mockTypeTattooInsert.type);
+          
+        })
+        .expect(200, done);
+    });
+    it('should update a TypeTattoo', function (done) {
+      request(sails.hooks.http.app)
+        .post('/typetattoo/update')
+        .send(mockTypeTattooUpdate)
+        .expect(function(res){})
+        .expect(200, done);
+    });
+    it('should get a TypeTattoo Updated', function (done) {
+      request(sails.hooks.http.app)
+        .post('/typetattoo/getOne')
+        .send({id: mockTypeTattooUpdate.id})
+        .expect(function(res) {
+          assert.equal(res.body.id, mockTypeTattooUpdate.id);
+          assert.equal(res.body.type, mockTypeTattooUpdate.type);        })
+        .expect(200, done);
+    });
+    it('should delete a TypeTattoo', function (done) {
+      request(sails.hooks.http.app)
+      .delete('/typetattoo/delete')
+      .send({id: mockTypeTattooUpdate.id})
+      .expect(200, done);
+    }); 
+    it('Should get a []', function (done) {
+      request(sails.hooks.http.app)
+        .post('/typetattoo/getOne')
+        .send({id: mockTypeTattooUpdate.id})
+        .expect([])
+        .expect(200, done);
+       
+    }); 
   });
+
 });
