a10e3e32ef42097f76bd09c9c9cd2bc9ccbbc77a
edwinvarroyo <e28v11a96@gmail.com>
2017-05-24 15:26:37 -0500

    #9 add Quotation model

---
+++ api/models/Quotation.js
@@ -0,0 +1,55 @@
+/**
+ * Quotation.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	dimensionsX: {
+      type: 'float',
+      size: 5,
+      required: true
+    },
+    dimensionsY: {
+      type: 'float',    
+      size: 5,
+      required: true
+    },
+    style:{
+      model: 'style'
+    },
+    comments:{
+      type: "string",
+      maxLength: 265
+    },
+    userId:{
+      model: "user"
+    },
+    references: {
+      collection: 'quotationreferences',
+      via: 'quotation'
+    }
+  },
+  beforeUpdate: function (values, cb) { 
+    if(values.style){
+      Style.findOne({ id: values.style }).exec(function (err, style){
+        if (err) {
+          return cb(err);
+        }
+        if (!style) {
+          cb('Could not find Style, sorry.');
+        }
+        if(style){
+          values.style= style.id;
+          cb();
+        } 
+      }); 
+    }
+    else{
+      cb();
+    }
+  }
+};
