5363a57b68bcda2c7760afd40b5af01116759ce9
edwinvarroyo <e28v11a96@gmail.com>
2017-05-24 15:22:22 -0500

    #9 add Element Controller

---
+++ api/controllers/ElementController.js
@@ -0,0 +1,10 @@
+/**
+ * ElementController
+ *
+ * @description :: Server-side logic for managing Elements
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+ 
+};
