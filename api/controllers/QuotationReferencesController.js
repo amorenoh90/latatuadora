aafc631e785443357f25fc1b88bdec04ceea1abe
edwinvarroyo <e28v11a96@gmail.com>
2017-05-24 15:23:13 -0500

    #9 add Quotation References Controller

---
+++ api/controllers/QuotationReferencesController.js
@@ -0,0 +1,11 @@
+/**
+ * QuotationReferencesController
+ *
+ * @description :: Server-side logic for managing Quotationreferences
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
