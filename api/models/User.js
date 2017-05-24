9abb07d14850b70db94181d0346444aa61d2212a
edwinvarroyo <e28v11a96@gmail.com>
2017-05-24 15:28:06 -0500

    #9 add User model

---
+++ api/models/User.js
@@ -0,0 +1,72 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+var bcrypt = require('bcrypt');
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string',
+      maxLength: 40,
+      alphanumeric: true, 
+      required: true
+    },
+    lastname: {
+      type: "string",
+      alphanumeric: true
+    },
+    email:{
+      type: 'email',
+      unique: true,
+      required: true
+    },
+    password:{
+      type: "string",
+      minLength: 8
+    },
+    certCofepris: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    id: {
+      autoIncrement: true,
+      type: 'integer',
+      unique: true,
+      primaryKey: true
+    },
+    telephone:{
+      type: "integer",
+      maxLength: 10,
+      minLength: 8
+    },
+    city:{
+      type: "string"
+    },
+    userType:{
+      model: "usertype"
+    },
+    quotation:{
+      collection: "quotation",
+      via: "userId"
+    }
+  },
+
+  beforeCreate: function (values, cb) { 
+    if(values.password){
+      bcrypt.hash(values.password, 10, function(err, hash) {
+        if(err) return cb(500, err);
+        values.password = hash;
+        cb();
+      });
+    }
+    else{
+      cb();
+    }
+  }
+};
+
