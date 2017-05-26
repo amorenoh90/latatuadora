4b8bd9a91aec7d0b7062d707f7f28318409c1709
edwinvarroyo <e28v11a96@gmail.com>
2017-05-26 17:44:48 -0500

    #10 add Studio Style model

---
+++ api/models/StudioStyle.js
@@ -0,0 +1,19 @@
+/**
+ * StudioStyle.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	studioId:{
+  		model: "studio"
+  	},
+  	styleId:{
+  		model: "style"
+  	}
+  }
+};
+
