ee92a99d439226c6a1369d9dcb5fd09cb7771b62
edwinvarroyo <e28v11a96@gmail.com>
2017-06-02 16:06:35 -0500

    #11 add element to create Token

--- api/services/jwt.js
+++ api/services/jwt.js
@@ -1,15 +1,15 @@
-
 var jwt = require('jwt-simple');
 var moment = require('moment');
-var SECRET_TOKEN = 'latatuadoratokensecret';
+var constants = require('../Constants');
 module.exports = {
-	createToken: function (user, done) {
-		const payload = {
-			sub: user.id,
-			iat: moment().unix(),
-			exp: moment().add(7, 'days').unix()
-		}
+    createToken: function (user, done) {
+        const payload = {   
+            sub: user.id,
+            typ: user.userType,
+            iat: moment().unix(),
+            exp: moment().add(7, 'days').unix()
+        }
 
-		return jwt.encode(payload, SECRET_TOKEN);
-	}
+        return jwt.encode(payload, constants.SECRET_TOKEN);
+    }
 }
