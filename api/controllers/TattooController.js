1ec9b6e728cf167d53c81cb59eaa7f9214c40b97
edwinvarroyo <e28v11a96@gmail.com>
2017-06-02 18:00:56 -0500

    #13 add Add function for create new tattoo

--- api/controllers/TattooController.js
+++ api/controllers/TattooController.js
@@ -7,5 +7,26 @@
 
 
 module.exports = {
-  
+    add: function(req, res) {
+        Tattoo.create(req.body).exec(function (err, tattoo){
+          if (err) { 
+            return res.serverError(err); 
+          }
+          else{
+            Tattoo.addImg(req.file, tattoo.id, function (err, done) {
+              if(err){
+                res.badRequest(err);
+              }
+              else{
+                if(done === null){
+                  return res.send(tattoo)
+                }
+                else{
+                  return res.send(done)
+                }
+              }
+            })
+          }
+        });
+    }
 };
