e82e847931449812fe067de6cacddade770fdd33
edwinvarroyo <e28v11a96@gmail.com>
2017-06-02 16:08:03 -0500

    #11 add Constats file

---
+++ api/Constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+    userType:{
+        admin: 1,
+        user: 2,
+        studio: 3,
+        freelance: 4,
+        quotient: 5
+    },
+    SECRET_TOKEN : 'latatuadoratokensecret'
+};
