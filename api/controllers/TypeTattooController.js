4c911bbbbd5e7abaa58723a1fa07982ebe6e868a
edwinvarroyo <e28v11a96@gmail.com>
2017-06-02 18:01:14 -0500

    #13 delete TypeTattoo controller

--- api/controllers/TypeTattooController.js
+++
@@ -1,9 +0,0 @@
-/**
- * TypeTattooController
- *
- * @description :: Server-side logic for managing Typetattoos
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
-module.exports = {
-  
-};
