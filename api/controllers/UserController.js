b8b2d39ee5513528fd0658841c55107bc5cc0291
edwinvarroyo <e28v11a96@gmail.com>
2017-06-02 16:04:16 -0500

    #11 add loguo method for User, Studio and Freelancer

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,53 +1,36 @@
 /**
  * UserController
- *
- * @description :: Server-side logic for managing users
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
+var constants = require('../Constants');
 module.exports = {
-	logup: function(req, res) {
-		var newuser={
-			name: req.body.name,
-			email: req.body.email,
-			password: req.body.password
-		}
-		User.signUp(newuser, function (err, user) {
-			if(err) res.negotiate(err);
-			else{
-				if(!user){
-					res.notFound(err);
-				}
-				else{
-					res.status(200).send({token: jwt.createToken(newuser)});
-				}
-			}
-		})
-	},
-	login: function(req, res) {
-		var user ={
-			email: req.body.email,
-			password: req.body.password
-		}
+    logup: function (req, res) {
+      logupService.add(req.body, req.file, function (done) {
+        res.send({token: done});
+      })
+    },
+    login: function(req, res) {
+        var loginuser = {
+            email: req.body.email,
+            password: req.body.password
+        }
 
-		if(user.email && user.password){
-			User.attemptLogin({ email: req.body.email, password: req.body.password}, function (err, user) {
-				if(!err){
-					if(!user) {
-						res.badRequest("Invalid Email/Password combination");
-					}
-					else{
-						res.status(200).send({token: jwt.createToken(user)});	
-					}
-				}
-				else{
-					res.negotiate(err);
-				}
-			})
-		}
-		else{
-			res.badRequest('Email and Password are required');
-		}
-	}
+        if(loginuser.email && loginuser.password){
+            User.attemptLogin(loginuser, function (err, user) {
+                if(!err){
+                    if(!user) {
+                        res.badRequest("Invalid Email/Password combination");
+                    }
+                    else{
+                        res.status(200).send({token: jwt.createToken(user)}); 
+                    }
+                }
+                else{
+                    res.negotiate(err);
+                }
+            })
+        }
+        else{
+            res.badRequest('Email and Password are required');
+        }
+    }
 };
-
