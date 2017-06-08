9e5ff3132727bc65953a14dae8a985b1d46b9b3b
Juan Resendiz <jresendiz27@gmail.com>
2017-06-08 13:39:09 -0500

    #9, changing max calculation retrieval

--- api/services/Quotient.js
+++ api/services/Quotient.js
@@ -8,37 +8,30 @@ module.exports = {
 
     if(area > 20) area = 20; //provicional
 
-    Calculator.find({styleId: quotation.styleId, minRange:{'<=': area}, maxRange:{'>=': area} }).exec(function (err, records){
+    Calculator.find({styleId: quotation.styleId}).exec(function (err, records){
       if (err) {
         done(err);
       }
       else{
-        if(records.length>1){
-          var sumin=0, sumax=0;
-          for(i in records){
-            sumin = sumin + records[i].minAmount;
-            sumax = sumax + records[i].maxAmount;
-          }
-          var minprom = (sumin/records.length).toFixed(2);
-          var maxprom = (sumax/records.length).toFixed(2);
-          if(minprom>maxprom){
-            results.minAmount = maxprom;
-            results.maxAmount = minprom;
-          }
-          else{
-            results.minAmount = maxprom;
-            results.maxAmount = minprom;
-          }
-          done(null, results);
-        }
-        if(records.length == 1){
-          results.minAmount = records[0].minAmount;
-          results.maxAmount = records[0].maxAmount;
-          done(null, results);
-        }
+        var minAmmount, maxAmmount;        
         if(records.length == 0){
           done(null,{message: "no data for your quotation"});
+          return;
         }
+        minAmmount = records[0].minAmount;
+        maxAmmount = records[0].maxAmount;
+        var sumin=0, sumax=0;
+        for(i in records){
+          if(minAmount > records[0].minAmount) {
+              minAmmount = records[i].minAmount;
+          } 
+          if(maxAmount > records[0].maxAmount) {
+              maxAmmount = records[i].maxAmount;
+          }
+        }
+        results.minAmount = minAmmount;
+        results.maxAmount = maxAmmount;
+        done(null, results);                 
       }
     });    
   }
