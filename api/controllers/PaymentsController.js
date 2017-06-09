059f35655b48e8c4e9923dfc6488437de16b3c6a
edwinvarroyo <e28v11a96@gmail.com>
2017-06-09 17:56:01 -0500

    #16 add paypal functions

--- api/controllers/PaymentsController.js
+++ api/controllers/PaymentsController.js
@@ -7,6 +7,12 @@
 var conekta = require('conekta');
  conekta.api_key = 'key_poWJzs7PVwgKD8eAFqdnYw';
  conekta.api_version = '2.0.0';
+var paypal = require('paypal-rest-sdk');
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live 
+  'client_id': 'AWCKGzQv2c29C-zyzlEFpAz5GljvGPbXqSptzJw7V4BuEQFMle3j54kYPFQLM-z-ug-IycznJvdngzns',
+  'client_secret': 'EKOvENrICZilSHCE44wTyVZZQbwDsBoY5Hzk_UTbAW4LG3WLOtC79zbMZz7UTCLzAPnL7lNDvUGhmMZH'
+});
 module.exports = {
     conekta: function(req, res) {
         conekta.Customer.create({
@@ -54,6 +60,74 @@ module.exports = {
                 });
             }
         });
+    },
+    paypalIntent: function (req, res) {
+        var payment = {
+            intent: "sale",
+            payer: {
+                payment_method: "paypal"
+            },
+            redirect_urls: {
+                return_url: "http://localhost:1337/paypalexecute",
+                cancel_url: "http://localhost:1337/paypalcancel"
+            },
+            transactions: [{
+                item_list: {
+                    items: [{
+                        name: req.body.item,
+                        sku: "tatooflorshurado",
+                        price: req.body.price,
+                        currency: "MXN",
+                        quantity: 1
+                    }]
+                },
+                amount: {
+                    currency: "MXN",
+                    total: req.body.price
+                },
+                description: "This is the payment description."
+            }]
+        }
+        paypal.payment.create(payment, function (error, payment) {
+          if (error) {
+            res.serverError(err);
+          } else {
+            if(payment.payer.payment_method === 'paypal') {
+              req.session.paymentId = payment.id;
+              var redirectUrl;
+              for(var i=0; i < payment.links.length; i++) {
+                var link = payment.links[i];
+                if (link.method === 'REDIRECT') {
+                  redirectUrl = link.href;
+                }
+              }
+              res.redirect(redirectUrl);
+            }
+          }
+        });
+
+    },
+    paypalExecute: function (req, res) {
+        var paymentId = req.session.paymentId;
+        var payerId = req.param('PayerID');
+
+        var details = { "payer_id": payerId };
+        paypal.payment.execute(paymentId, details, function (error, payment) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("paid", payment);
+                console.log(payment.transactions[0].amount);
+                res.send({
+                    message: "Paid",
+                    amount: payment.transactions[0].amount,
+                    items: payment.transactions[0].item_list.items
+                });
+            }
+        });
+    },
+    paypalCancel: function (req, res) {
+        res.send({message: "The payment got canceled"});
     }
 };
 
