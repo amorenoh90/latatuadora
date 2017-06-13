7c2ee5d9c4150b007582bcd6d47552538424f2a6
edwinvarroyo <e28v11a96@gmail.com>
2017-06-13 15:39:13 -0500

    #17 add constants file

---
+++ api/Constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+    userType:{
+        admin: 1,
+        user: 2,
+        studio: 3,
+        freelance: 4,
+        quotient: 5
+    },
+    SECRET_TOKEN : 'latatuadoratokensecret',
+    payment:{
+    	pending: 1,
+    	success: 2,
+    	expired: 3,
+    	undefined: 4
+    }
+};
