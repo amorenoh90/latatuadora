f1f22eb1b022eb5bd9cf595fafefd0722324fe7b
edwinvarroyo <e28v11a96@gmail.com>
2017-06-14 18:04:04 -0500

    #18 add Favorite tattoo controller test

---
+++ test/integration/controllers/FavoriteTattooController.test.js
@@ -0,0 +1,49 @@
+var request = require('supertest'),
+  assert = require('assert');
+
+describe('FavoriteTattooController', function() {
+
+  var mockuser = {
+        form: "user",
+        name: "usertest",
+        email: "asdsdsddd@sdfvffdggz.com",
+        password: "password"
+      },
+      mocktattoo = {};
+    it("should logup a normal User and create a Tatto (prerequisites)", function (done) {
+      request(sails.hooks.http.app)
+      .post('/logup')
+      .send(mockuser)
+      .expect(function(res) {
+        mockuser.token = res.body.token;
+        assert.notEqual(res.body, null);
+        Tattoo.create({name:'tattootest'}).exec(function (err, tattoo){
+          if (err) { done(err); }
+          else{
+            mocktattoo.tattooId = tattoo.id;
+          }
+        });
+      })
+      .expect(200, done);
+    });
+    it("should create favorite Tatto", function (done) {
+      request(sails.hooks.http.app)
+      .post('/tattoofav')
+      .set('Authorization', mockuser.token)
+      .send(mocktattoo)
+      .expect(function(res) {
+        assert.equal(res.body.message, 'favorite is added');
+      })
+      .expect(200, done); 
+    });
+    it("should remove favorite Tatto", function (done) {
+      request(sails.hooks.http.app)
+      .post('/removetattoofav')
+      .set('Authorization', mockuser.token)
+      .send(mocktattoo)
+      .expect(function(res) {
+        assert.equal(res.body.message, 'favorite is removed');
+      })
+      .expect(200, done); 
+    });
+});   
