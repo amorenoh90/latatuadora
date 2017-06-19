29f710f46bde42e0e3d1f8af1341b5cee2ee7f41
edwinvarroyo <e28v11a96@gmail.com>
2017-06-19 17:37:06 -0500

    #20 adjust Tattoo controller test

--- test/integration/controllers/TattooController.test.js
+++ test/integration/controllers/TattooController.test.js
@@ -18,13 +18,13 @@ describe('TattooController', function() {
       .post('/tattoo')
       .send(mocktattoo)
       .expect(function(res) {
-        assert.notEqual(res.body[0].id, null);
-        assert.equal(res.body[0].dimensionsY, mocktattoo.dimensionsY);
-        assert.equal(res.body[0].dimensionsX, mocktattoo.dimensionsX);
-        assert.equal(res.body[0].name, mocktattoo.name);
-        assert.equal(res.body[0].element, mocktattoo.element);
-        assert.equal(res.body[0].partbody, mocktattoo.partbody);
-        assert.equal(res.body[0].style, mocktattoo.style);
+        assert.notEqual(res.body.id, null);
+        assert.equal(res.body.dimensionsY, mocktattoo.dimensionsY);
+        assert.equal(res.body.dimensionsX, mocktattoo.dimensionsX);
+        assert.equal(res.body.name, mocktattoo.name);
+        assert.equal(res.body.element, mocktattoo.element);
+        assert.equal(res.body.partbody, mocktattoo.partbody);
+        assert.equal(res.body.style, mocktattoo.style);
       })
       .expect(200, done);
     });
