cc53db8cfb0c4dfc02b709294807dfda597a4603
edwinvarroyo <e28v11a96@gmail.com>
2017-06-20 16:25:31 -0500

    #20 add Recurrent controller

---
+++ api/controllers/RecurrentController.js
@@ -0,0 +1,11 @@
+/**
+ * RecurrentController
+ *
+ * @description :: Server-side logic for managing Recurrents
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
