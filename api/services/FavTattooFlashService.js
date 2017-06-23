5d2228229ac5d9b0f39b42e66351d8743a0bbaba
edwinvarroyo <e28v11a96@gmail.com>
2017-06-23 18:41:24 -0500

    19 add favorite flash and tattoo service

---
+++ api/services/FavTattooFlashService.js
@@ -0,0 +1,26 @@
+module.exports = {
+  find: function (values, done) {
+    var favs = {};
+    FavoriteFlash.find({userId:values.user.id}).populate('flashId')
+      .then(function (favorites){
+            favs.flashes = favorites;
+            return favs;
+            console.log("flash",favs);
+        })
+      .then(function (favs) {
+        FavoriteTattoo.find({userId : values.user.id}).populate('tattooId')
+          .exec(function (err, favtattoo){
+            if (err) {
+              return done(err);
+            }
+            else{
+              favs.tattoos = favtattoo;
+              return done(null, favs); 
+            }
+        });
+      })
+      .catch(function (err) {
+        return done(err);
+      });
+  }
+}
