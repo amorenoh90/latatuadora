fc7566422f5d4b2571eced5acc9b6e40eddd3cd0
edwinvarroyo <e28v11a96@gmail.com>
2017-06-30 16:14:24 -0500

    #22 fix bugs

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -3,37 +3,39 @@
  */
 var constants = require('../Constants');
 module.exports = {
-    logup: function (req, res) {
-      logupService.add(req.body, req.file, function (err, done) {
-        if(err){
-            res.badRequest(err);
-        }
-        res.send({token: done});
-      })
-    },
-    login: function(req, res) {
-        var loginuser = {
-            email: req.body.email,
-            password: req.body.password
-        }
+  logup: function (req, res) {
+    logupService.add(req.body, req.file, function (err, done) {
+    if(err){
+      res.badRequest(err);
+    }
+    else{
+      res.send({token: done});   
+    }
+    })
+  },
+  login: function(req, res) {
+    var loginuser = {
+      email: req.body.email,
+      password: req.body.password
+    }
 
-        if(loginuser.email && loginuser.password){
-            User.attemptLogin(loginuser, function (err, user) {
-                if(!err){
-                    if(!user) {
-                        res.badRequest("Invalid Email/Password combination");
-                    }
-                    else{
-                        res.status(200).send({token: jwt.createToken(user), usertype: user.userType}); 
-                    }
-                }
-                else{
-                    res.negotiate(err);
-                }
-            })
+    if(loginuser.email && loginuser.password){
+      User.attemptLogin(loginuser, function (err, user) {
+        if(!err){
+          if(!user) {
+            res.badRequest("Invalid Email/Password combination");
+          }
+          else{
+            res.send({token: jwt.createToken(user), usertype: user.userType}); 
+          }
         }
         else{
-            res.badRequest('Email and Password are required');
+          res.negotiate(err);
         }
+      })
     }
+    else{
+      res.badRequest('Email and Password are required');
+    }
+  }
 };
