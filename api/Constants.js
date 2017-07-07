085fca36550b2c09a6ffe3cc367192d97697e645
edwinvarroyo <e28v11a96@gmail.com>
2017-07-07 17:49:19 -0500

    #22 add membership status constants

--- api/Constants.js
+++ api/Constants.js
@@ -11,22 +11,28 @@ module.exports = {
         pending: 1,
         paid: 2,
         expired: 3,
-        undefined: 4
+        canceled:4,
+        undefined: 5
     },
     payPalUrls:{
         execute: 'http://localhost:1337/paypalexecute',
         cancel: 'http://localhost:1337/paypalcancel',
-        aceptplan: 'http://localhost/aceptplan',
-        cancelplan: 'http://localhost/cancelplan'
+        aceptplan: 'http://localhost:1337/aceptplan',
+        cancelplan: 'http://localhost:1337/cancelplan'
     },
     itemType:{
         1: "Flash",
-        2: "Lead",
-        3: "StudioMembership", 
-        4: "FreelanceMembership"
+        2: "StudioMembership", 
+        3: "FreelanceMembership",
+        4: "Leads"
     },
     memberships:{
         studio: 1,
         freelancer: 2
+    },
+    membershipStatus:{
+        active: 1,
+        inactive: 2,
+        locked: 3
     }
 };  
