5cbe044fb28ddb507d77ea2c32caf851f4cbedde
edwinvarroyo <e28v11a96@gmail.com>
2017-07-07 17:58:42 -0500

    #22 fixed bugs for update items

--- api/services/ConektaService.js
+++ api/services/ConektaService.js
@@ -11,19 +11,14 @@ paypal.configure({
 var request = require('request');
 var comprapagotoken = 'sk_test_4327294905c54c2065';
 module.exports = {
-  charge: function (values, auth, done) {
-    User.findOne({id: auth.id})
-      .then(function (user){
-        if(!user){
-          return done({message: 'User not found'});
+  buyFlash: function (values, auth, done) {
+    if(auth.conekta === null){
+      ConektaService.createCostumer(values, auth, function (err, customer) {
+        if(err){
+          return done(err);
         }
         else{
-          return user;
-        }
-      })
-      .then(function (user) {
-        if(user.conekta){
-          PaymentsService.order(values, user, function (err, order) {
+          ConektaService.order(customer, values, function (err, order) {
             if(err){
               return done(err);
             }
@@ -32,56 +27,79 @@ module.exports = {
             }
           });
         }
-        else{ 
-          PaymentsService.createCostumer(user, values, function (err, customer) {
+      });
+    }
+    else{
+      ConektaService.order(values, auth, function (err, order) {
+        if(err){
+          return done(err);
+        }
+        else{
+          return done(null, order);
+        }
+      });
+    }
+  },
+  buyMembership: function (values, auth, done) {
+    if(auth.conekta === null){
+      ConektaService.createCostumer(values, auth, function (err, customer) {
+        if(err){
+          return done(err);
+        }
+        else{
+          ConektaService.subscribe(values, customer, function (err, subscription) {
             if(err){
               return done(err);
             }
             else{
-              console.log(customer);
-              PaymentsService.order(values, customer, function (err, order) {
-                if(err){
-                  return done(err);
-                }
-                else{
-                  return done(null, order);
-                }
-              });
+              return done(null, subscription);
             }
           });
         }
-      })
-      .catch(function (err) {
-        return done(err);
       });
+    }
+    else{
+      ConektaService.subscribe(values, auth, function (err, subscription) {
+        if(err){
+          return done(err);
+        }
+        else{
+          return done(null, subscription);
+        }
+      });
+    }
   },
   order: function (values, user, done) {
-    ItemsFinderService.find(values, function (err, item) {
-      if(err){
-        return done(err);
-      }
-      else{
-        item.amount = item.amount*100;
+    Flash.findOne({ id: values.itemId })
+      .then(function (flash){
+        if (!flash) {
+          return done('Could not find Flash, sorry.');
+        }
+        else{
+          return flash; 
+        }
+      })
+      .then(function (flash) {
+        flash.amount = flash.amount*100;
         conekta.Order.create({
-          line_items: [{
-            name: item.id,
-            unit_price: item.amount,
-            quantity: 1
-          }],
           currency: "MXN",
           customer_info: {
             customer_id: user.conekta
           },
+          line_items: [{
+            name: 'flash'+ flash.id,
+            unit_price: flash.amount,
+            quantity: 1
+          }],
           charges: [{
             payment_method: {
               type: "default"
             }
           }]
-        }, function(err, order) {
+        },function(err, order) {
           if(err){
             return done(err);
           }
-          else{
             var pay = order.toObject();
             Payments.create({
               user: user.id,
@@ -89,48 +107,41 @@ module.exports = {
               status: constants.payment[pay.payment_status],
               itemType: values.itemType,
               reference: values.itemId
-            })
-            .then(function (purchase){
+            }).exec(function (err, payment){
               if (err) { 
-                  return done(err);
+                return done(err);
               }
               else{
-                console.log(pay);
-                return pay;
-              }
-            })
-            .then(function (pay) {
-              var response = {
-                payment_status: pay.payment_status,
-                amount: pay.amount,
-                currency: pay.currency,
-                customer_info: pay.customer_info,
-                purchaseId: pay.id
-              };
-              console.log(response);
-              if(response.payment_status == 'paid'){
-               ItemsFinderService.update(values, function (err, updated) {
-                if(err){
-                  return done(err);
+                var response = {
+                  payment_status: pay.payment_status,
+                  amount: pay.amount/100,
+                  currency: pay.currency,
+                  customer_info: pay.customer_info,
+                  purchaseId: pay.id
+                };
+                if(response.payment_status == 'paid'){
+                  Flash.update({id: flash.id},{sell: true})
+                    .exec(function afterwards(err, updated){
+                      if (err) {
+                        return done(err);
+                      }
+                      else{
+                        return done(null, response);
+                      }
+                    }); 
                 }
-                else{
-                  return done(null, response);
+                else {
+                  return done(null, response);   
                 }
-               }); 
               }
-              else {
-                return done(null, response);   
-              }
-            })
-            .catch(function (err) {
-              return done(err);
-            });   
-          }
-        });
-      }
-    });
+            });
+        })
+      })
+      .catch(function (err) {
+        return done(err);
+      });
   },
-  createCostumer: function (user, values,  done) {
+  createCostumer: function (values, user, done) {
     conekta.Customer.create({
       name: values.cardname,
       email: user.email,
@@ -174,14 +185,13 @@ module.exports = {
   deletePaymentSource: function (user, values, done) {
     conekta.Customer.find(user.conekta, function(err, customer) {
       customer.payment_sources.get(0).delete(function(err, resp) {
-        console.log(resp.toObject());
       })
     });
   },
   createPlan: function (values, done) {
     cent = values.amount*100;
     conekta.Plan.create({
-      //"id": values.planId,
+      "id": values.planId,
       "name": values.name,
       "amount": cent,
       "currency": "MXN",
@@ -196,5 +206,56 @@ module.exports = {
         return done(null, plan);
       }
     });
+  },
+  subscribe: function (values, auth, done) {
+    Memberships.findOne({ id: auth.member.membership })
+      .then(function (membership){
+        if (!membership) {
+          return done('Could not find Membership, sorry.');
+        }
+        return membership;
+      })
+      .then(function (membership) {
+       customer = conekta.Customer.find(auth.conekta, function(err, customer) {
+          customer.createSubscription({
+            plan: membership.conekta
+          }, function(err, res) {
+            if(err){
+              return done(err);
+            }
+            else{
+              subscription = res.toObject();
+              var paymentstatus;
+              if(membership.status == 'active'){
+                paymentstatus = constants.payment.paid; 
+              }
+              else{
+                paymentstatus = constants.payment.pending;
+              }
+              Payments.create({
+                user: auth.id,
+                purchaseId: subscription.id,
+                status: paymentstatus,
+                itemType: values.itemType,
+                reference: values.itemId
+              }).exec(function (err, payment){
+                if (err) { 
+                  return done(err); 
+                }
+                else{
+                  var response = {
+                    status: subscription.status,
+                    purchaseId: subscription.id
+                  }
+                  return done(null, response);
+                }
+              });
+            }
+          });
+        });
+      })
+      .catch(function (err) {
+        return done(err);
+      });
   }
 }
