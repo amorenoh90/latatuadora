79dadf779bb6cfd51b302909fe84d4fbd1ea34fc
edwinvarroyo <e28v11a96@gmail.com>
2017-07-13 19:08:47 -0500

    #22 adjust timeout

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -6,7 +6,7 @@ var sails = require('sails');
 before(function(done) {
 
   // Increase the Mocha timeout so that Sails has enough time to lift.
-  this.timeout(10000);
+  this.timeout(100000);
 
   sails.lift({
     // configuration for testing purposes
