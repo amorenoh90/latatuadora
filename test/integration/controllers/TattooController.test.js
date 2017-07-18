448edef459b6d5ad040d46b362058a3dffb0e6bb
edwinvarroyo <e28v11a96@gmail.com>
2017-07-18 16:31:50 -0500

    #27 add Approve TattooController Test

--- test/integration/controllers/TattooController.test.js
+++ test/integration/controllers/TattooController.test.js
@@ -11,7 +11,13 @@ describe('TattooController', function() {
         partbody: 2,
         style: 1,
         artistId: 2
-      }
+      },
+      mockadmin={
+        email: 'admin@latatoadora.com',
+        name: 'admin',
+        password: 'password',
+        userType: 1
+      };
     
     it("should create a new Tattoo", function (done) {
       request(sails.hooks.http.app)
@@ -25,6 +31,37 @@ describe('TattooController', function() {
         assert.equal(res.body[0].element, mocktattoo.element);
         assert.equal(res.body[0].partbody, mocktattoo.partbody);
         assert.equal(res.body[0].style, mocktattoo.style);
+        mocktattoo.id = res.body[0].id;
+      })
+      .expect(200, done);
+    });
+    it("should create an login a user type admin", function (done) {
+      User.create(mockadmin)
+      .then(function (admin){
+        return admin;
+      })
+      .then(function (admin) {
+        request(sails.hooks.http.app)
+        .post('/login')
+        .send(mockadmin)
+        .expect(function (res) {
+          assert.notEqual(res.body.token, null);
+          mockadmin.token = res.body.token;
+        })
+        .expect(200, done);
+      })
+      .catch(function (err) {
+        done(err);
+      });
+    });
+    it("should approve tattoo", function (done) {
+      request(sails.hooks.http.app)
+      .put('/tattoo/approve/' + mocktattoo.id)
+      .set('X-Authorization', mockadmin.token)
+      .send({publicate: true})
+      .expect(function (res) {
+        assert.equal(res.body.id , mocktattoo.id);
+        assert.equal(res.body.publicate, true);
       })
       .expect(200, done);
     });
