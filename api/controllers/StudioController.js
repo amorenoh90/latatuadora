aee5a2f3967d50741dc35b22aec2345bfdda2809
edwinvarroyo <e28v11a96@gmail.com>
2017-07-18 16:56:39 -0500

    #28 add find function with publicate filter

--- api/controllers/StudioController.js
+++ api/controllers/StudioController.js
@@ -4,8 +4,17 @@
  * @description :: Server-side logic for managing Studios
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
+var constants = require('../Constants.js');
 module.exports = {
-	
+	find: function (req, res) {
+		Studio.find({status: constants.studioStatus.publicate}).exec(function (err, studios){
+      if (err) {
+        return res.serverError(err);
+      }
+      else{
+        res.send(studios);
+      }
+    });
+	}
 };
 
