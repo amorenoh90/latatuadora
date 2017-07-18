83c3cbd7a168954af743e48dd95e6ed0595db40b
edwinvarroyo <e28v11a96@gmail.com>
2017-07-17 20:32:03 -0500

    #26 adjust function for logged user

--- api/controllers/QuotationController.js
+++ api/controllers/QuotationController.js
@@ -8,80 +8,55 @@ module.exports = {
 /**
  * (POST /quotation)
  */
-  createquotation: function  (req, res) {
-    var newuser = {
-      name: req.body.name,
-      email: req.body.email,
-      telephone: req.body.telephone,
-      userType: constants.userType.quotient
-    };
-    User.findOrCreate({email: newuser.email},newuser).exec(function (err, user){
-      if (err){
-        res.serverError(err);
-      }
-        else{
-        var  quotation = {
-          dimensionsY : req.body.dimensionsY,
-          dimensionsX : req.body.dimensionsX,
-          comments: req.body.comments,
-          styleId: req.body.style,
-          bodypartId: req.body.bodypart,
-          studioId: req.body.studio,
-          userId: user.id
+  quotation: function (req, res) {
+    if(req.headers['x-authorization']){
+      var token = req.headers['x-authorization'];
+      jwt.verifyToken(token, function (err, decoded) {
+        if(err){
+          return res.forbidden({message : err.message});
         }
-
-        Quotation.create(quotation).exec(function (err, quotation){
-          if (err){
-            res.serverError(err);
-          }  
-          else{ 
-            req.file('reference').upload({
-              maxBytes: 10000000,
-              dirname: require('path').resolve(sails.config.appPath, 'assets/references/images')
-            },function (err, uploadedFiles) {
-              if (err) {
-                sails.log.error(err);
-              }
-              else{
-                for(i in uploadedFiles){
-                  QuotationReferences.create({imgUrl: uploadedFiles[i].fd, quotation: quotation.id}).exec(function (err, refrence){
-                    if (err) { return res.serverError(err); }
-                  });
+        if(decoded.typ != constants.userType.user){
+          return res.forbidden({message: 'This User Type not permitted to perform this action.'});
+        }
+        else{
+          User.findOne({id: decoded.sub}).exec(function (err, user){
+            if (err) {
+              return res.serverError(err);
+            }
+            if (!user) {
+              return res.notFound('Could not find User, sorry.');
+            }
+            else{
+              req.body.newUser = user;
+              req.body.file = req.file;
+              QuotationService.createquotation(req.body, function (err, quotation) {
+                if(err){
+                  return res.serverError(err);
                 }
-              }
-            });
-            if(!quotation.studioId){
-              Quotient.calculate(quotation, function (err, calculated) {
-                if(err) res.negotiate(err);
-                else {
-                  Style.findOne({id:quotation.styleId}).exec(function (err, style){
-                    if (err) {
-                      return res.serverError(err);
-                    }
-                    if (!style) {
-                      return res.notFound('Could not find Style, sorry.');
-                    }
-                    else{
-                      calculated.styleText = style.calculatorText;
-                      res.send(200, calculated); 
-                    }
-                  });
+                else{
+                  return res.send(quotation);
                 }
               });
             }
-            else{
-              res.ok({message: "Are you quoting with study"});
-            }
-          }
-        });
-      }
-    });
+          });
+        }
+      });
+    }
+    else{
+      var newuser = {
+        name: req.body.name,
+        email: req.body.email,
+        telephone: req.body.telephone,
+        userType: constants.userType.quotient
+      };
+      Quotation.create(newuser);
+    }
   },
   findByStudio: function (req, res) {
     var studio = req.headers.studio.id;
     Quotation.find({studioId: studio}).populate('styleId').populate('bodypartId').populate('userId').exec(function (err, quotations){
       if (err) {
-        return res.serverError(':v',err);
+        return res.serverError(err);
       }
       else{
         return res.send(quotations);
