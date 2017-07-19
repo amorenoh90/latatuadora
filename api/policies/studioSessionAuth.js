b6b28f582b5391cc79bedfe1008d318be3599a53
edwinvarroyo <e28v11a96@gmail.com>
2017-07-19 17:20:55 -0500

    #30 fix bugs studioSession

--- api/policies/studioSessionAuth.js
+++ api/policies/studioSessionAuth.js
@@ -6,38 +6,40 @@ module.exports = function(req, res, next) {
   var forbiddenmessage = 'You are not permitted to perform this action.';
   var token = req.headers["x-authorization"];
   if (token) { 
-    jwt.verifyToken(token, function (err, decoded) {
-      if(err){
-      return res.forbidden({message : err.message});
+  jwt.verifyToken(token, function (err, decoded) {
+    if(err){
+    return res.forbidden({message : err.message});
+    }
+    else{
+    if(decoded.typ == constants.userType.studio){
+      var user = {
+      id: decoded.sub
       }
-      else{
-        if(decoded.typ == constants.userType.studio){
-          var user = {
-            id: decoded.sub
-          }
-          req.headers.user = user;
-          Studio.findOne({userId: user.id})
-          .then(function (studio){
-              req.headers.studio = studio;
-              return studio;
-          })
-          .then(function (studio) {
-            if(req.body){
-              req.body.studio = studio.id;
-            }
-            return next();
-          })
-          .catch(function (err) {
-            return res.serverError(err);
-          });
+      req.headers.user = user;
+      Studio.findOne({userId: user.id})
+      .then(function (studio){
+        if(!studio){
+          res.notFound({message : 'Could not find Studio, sorry.'});
         }
         else{
-          return res.forbidden({message: 'This User Type not permitted to perform this action.'})
+          req.headers.studio = studio;
+          if(req.body){
+            req.body.studio = studio.id;
+          }
+          return next();
         }
+      })
+      .catch(function (err) {
+        return res.serverError(err);
+      });
     }
-    });
+    else{
+      return res.forbidden({message: 'This User Type not permitted to perform this action.'})
+    }
+  }
+  });
   }
   else{
-    return res.forbidden({message : forbiddenmessage}); 
+   return res.forbidden({message : forbiddenmessage}); 
   }
 };
