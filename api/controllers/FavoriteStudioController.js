233c0398f1541095dea57cd406a8b8886a68fe95
edwinvarroyo <e28v11a96@gmail.com>
2017-07-18 19:26:40 -0500

    #29 fix bug consult function

--- api/controllers/FavoriteStudioController.js
+++ api/controllers/FavoriteStudioController.js
@@ -25,7 +25,7 @@ module.exports = {
         return res.serverError(err);
       }
       else{
-        return res.send(favorites[0]); 
+        return res.send(favorites); 
       }
     });
   },
