c8f2dcf583b7dec1f1e5528cdbbddaa5b0ae6c0b
Juan Resendiz <jresendiz27@gmail.com>
2017-08-28 12:58:03 -0500

    Adding favorite count for tattoos in controller

--- api/controllers/FavoriteTattooController.js
+++ api/controllers/FavoriteTattooController.js
@@ -38,6 +38,16 @@ module.exports = {
         return res.send({message: 'favorite is removed'});
       }
     });
+  },
+  countFavorites: function (req, res) {
+    var values = {
+      tattooId: req.query.tattooId
+    };
+    FavoriteTattoo.count(values).then(function (numberOfFavorites) {
+      res.send({count: numberOfFavorites});
+    }).catch(function (err) {
+      res.serverError(err);
+    });
   }
 };
 
