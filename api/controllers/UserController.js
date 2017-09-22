ee9b7f384c1cf333b176a9732186a99f46e630b9
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-09-22 17:00:51 -0500

    #28 - Added user edition methods

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,6 +1,187 @@
-/**
- * UserController
- */
+var response;
+
+function fillJSON(values, properties) {
+  var json = {};
+
+  for (var i = 0; i < properties.length; i++) {
+    var property = properties[i];
+
+    if (values[property]) {
+      json[property] = values[property];
+    }
+  }
+
+  return json;
+}
+
+function done(err, result) {
+  if (err) {
+    response.badRequest(err);
+  } else {
+    response.send({
+      token: result
+    });
+  }
+}
+
+function editUser(values, token) {
+  var userEditionProps = [
+    "name",
+    "email",
+    "password",
+    "telephone",
+    "userType"
+  ];
+  var userEdition = fillJSON(values, userEditionProps);
+  User.update({
+    id: values.id
+  }, userEdition, function (err, user) {
+    if (err) {
+      return done(err);
+    } else {
+      if (!user) {
+        return done(err);
+      } else {
+        return done(null, JWT.createToken(user));
+      }
+    }
+  });
+}
+
+function editStudio(values, image, token) {
+
+  var studioUserEditionProps = [
+      "name",
+      "email",
+      "password",
+      "telephone",
+    ],
+    studioEditionProps = [
+      "name",
+      "certCofepris",
+      "about",
+    ],
+    addressStudioProps = [
+      "street",
+      "numInt",
+      "numExt",
+      "lat",
+      "long",
+      "zc",
+      "state",
+      "town",
+      "suburb"
+    ],
+    studioId,
+    studioUserEdition = fillJSON(values, studioUserEditionProps),
+    studioEdition = fillJSON(values, studioEditionProps),
+    addressStudio = fillJSON(values, addressStudioProps);
+  studioUserEdition.userType = constants.userType.studio;
+  AddressService.add(addressStudio, function (err, newaddressId) {
+    if (err) {
+      return done(err);
+    } else {
+      User.update({
+        id: values.user
+      }, studioUserEdition, function (err, studioUser) {
+        if (err) {
+          return done(err);
+        } else {
+          if (!studioUser) {
+            return done(err);
+          } else {
+            studioEdition.userId = studioUser.id;
+            studioEdition.addressId = newaddressId;
+            Studio.update({
+              id: values.studio
+            }, studioEdition).exec(function (err, studio) {
+              if (err) {
+                return done(err);
+              } else {
+                for (i in values.schedule) {
+                  values.schedule[i].studioId = studio.id;
+                  Schedule.update({
+                      id: studioUser.schedule
+                    }, values.schedule[i])
+                    .exec(function (err, shedule) {
+                      if (err) {
+                        return done(err);
+                      }
+                    });
+                  for (i in values.styles) {
+                    values.styles[i].studioId = studio.id;
+                    StudioStyle.update({
+                        id: studioUser.stu
+                      }, values.styles[i])
+                      .exec(function (err) {
+                        if (err) {
+                          return done(err);
+                        }
+                      });
+                  }
+                }
+                Studio.addProfileImg(image, studio.id, function (cb) {
+                  done(null, JWT.createToken(studioUser));
+                });
+              }
+            });
+          }
+        }
+      });
+    }
+  });
+}
+
+function editFreelancer(values, image, token) {
+  values.userType = constants.userType.freelance;
+  var newfreelancerUserProps = [
+      "name",
+      "email",
+      "password",
+      "telephone",
+      "userType"
+    ],
+    newfreelancerProps = [
+      "name",
+      "about",
+      "fbUrl",
+      "twUrl",
+      "insUrl"
+    ];
+
+  var newfreelancerUser = fillJSON(values, newfreelancerUserProps),
+    newfreelancer = fillJSON(values, newfreelancerProps);
+  User.update({
+    id: values.id
+  }, newfreelancerUser).exec(function (err, freelanceruser) {
+    if (err) {
+      return done(err);
+    } else {
+      freelanceruser = freelanceruser[0];
+      newfreelancer.user = freelanceruser.id;
+      Freelancer.update({
+        user: newfreelancer.user
+      }, newfreelancer).exec(function (err, freelancer) {
+        if (err) {
+          return done(err);
+        } else {
+          for (i in values.zones) {
+            values.zones[i].freelancerId = freelancer.id;
+            Zone.create(values.zones[i]).exec(function (err) {
+              if (err) {
+                return done(err);
+              }
+            });
+          }
+          Freelancer.addProfileImg(image, freelancer.id, function (cb) {
+            return done(null, JWT.createToken(freelanceruser));
+          });
+        }
+      });
+    }
+  });
+}
+
 var constants = require('../Constants');
 var baseQuery = "select " +
   "User.name," +
@@ -14,14 +195,36 @@ var baseQuery = "select " +
   "User.conekta," +
   "User.createdAt," +
   "User.updatedAt" +
-  " from User where User.userType in ("+constants.userType.studio+", "+constants.userType.freelance+") ";
+  " from User where User.userType in (" + constants.userType.studio + ", " + constants.userType.freelance + ") ";
+
+
 module.exports = {
+  edit: function (req, res) {
+    var values = req.allParams(),
+      image = req.file,
+      token;
+    response = res;
+    if (values.form == 'user') {
+      editUser(values, token);
+    }
+    if (values.form == 'studio') {
+      editStudio(values, image, token);
+    }
+    if (values.form == "freelancer") {
+      editFreelancer(values, image, token);
+    }
+    if (!values.form) {
+      done(null, "No form specified");
+    }
+  },
   logup: function (req, res) {
     LogUpService.add(req.body, req.file, function (err, done) {
       if (err) {
         res.badRequest(err);
       } else {
-        res.send({token: done});
+        res.send({
+          token: done
+        });
       }
     })
   },
@@ -36,7 +239,10 @@ module.exports = {
           if (!user) {
             res.badRequest("Invalid Email/Password combination");
           } else {
-            res.send({token: JWT.createToken(user), usertype: user.userType});
+            res.send({
+              token: JWT.createToken(user),
+              usertype: user.userType
+            });
           }
         } else {
           res.negotiate(err);
@@ -60,7 +266,7 @@ module.exports = {
   findBy: function (req, res) {
     var nameOrEmailQuery = baseQuery + " AND (LOWER(CONCAT(TRIM(User.name),' ',TRIM(User.lastname))) like ? OR LOWER(User.email) like ?)";
     var nameOrEmailParam = req.param('queryParam') || '';
-    nameOrEmailParam = "%" +  nameOrEmailParam.toLowerCase().trim() + "%";
+    nameOrEmailParam = "%" + nameOrEmailParam.toLowerCase().trim() + "%";
     var escapedValues = [nameOrEmailParam, nameOrEmailParam];
     User.query(nameOrEmailQuery, escapedValues, function (err, results) {
       if (err) {
