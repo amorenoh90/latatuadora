8c808c17dd861d627aaec4fe133b46500d151438
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-09-25 14:03:59 -0500

    #36 Added getAll actions

--- api/controllers/ItineraryController.js
+++ api/controllers/ItineraryController.js
@@ -11,11 +11,119 @@ module.exports = {
         if (err) {
           res.serverError({
             err: err,
-            result: result
+            message: result.messages.pop()
           });
         } else {
           res.send({
-            result: result
+            message: result.messages.pop()
+          });
+        }
+      });
+  },
+
+  getAllAsFreelancer: function (req, res) {
+    var args = {
+        req: req,
+        input: {}
+      },
+      result = {};
+
+    req.headers.user = req.headers.user || 0;
+
+    args.input.jobber = req.headers.user.id;
+
+    ItineraryService
+      .getAll(args, function (err, result) {
+        if (err) {
+          res.serverError({
+            err: err,
+            message: result.messages.pop(),
+            itineraries: result.json_response.itineraries
+          });
+        } else {
+          res.send({
+            message: result.messages.pop(),
+            itineraries: result.json_response.itineraries
+          });
+        }
+      });
+  },
+
+  getAllAsStudio: function (req, res) {
+    var args = {
+        req: req,
+        input: {}
+      },
+      result = {};
+
+    req.headers.user = req.headers.user || 0;
+
+    args.input.jobber = req.headers.user.id;
+
+    ItineraryService
+      .getAll(args, function (err, result) {
+        if (err) {
+          res.serverError({
+            err: err,
+            message: result.messages.pop(),
+            itineraries: result.json_response.itineraries
+          });
+        } else {
+          res.send({
+            message: result.messages.pop(),
+            itineraries: result.json_response.itineraries
+          });
+        }
+      });
+  },
+
+  getAllAsClient: function (req, res) {
+    var args = {
+        req: req,
+        input: {}
+      },
+      result = {};
+
+    req.headers.user = req.headers.user || 0;
+
+    args.input.client = req.headers.user.id;
+
+    ItineraryService
+      .getAll(args, function (err, result) {
+        if (err) {
+          res.serverError({
+            err: err,
+            message: result.messages.pop(),
+            itineraries: result.json_response.itineraries
+          });
+        } else {
+          res.send({
+            message: result.messages.pop(),
+            itineraries: result.json_response.itineraries
+          });
+        }
+      });
+  },
+
+  getAllAsAdmin: function (req, res) {
+    var args = {
+        req: req,
+        input: {}
+      },
+      result = {};
+
+    ItineraryService
+      .getAll(args, function (err, result) {
+        if (err) {
+          res.serverError({
+            err: err,
+            message: result.messages.pop(),
+            itineraries: result.json_response.itineraries
+          });
+        } else {
+          res.send({
+            message: result.messages.pop(),
+            itineraries: result.json_response.itineraries
           });
         }
       });
