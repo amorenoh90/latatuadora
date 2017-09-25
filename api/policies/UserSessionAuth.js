51dbcfca87af79caf23a0f5770dcb31d16fe139d
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-09-25 16:37:42 -0500

    #36 - Implemented constants messages

--- api/policies/UserSessionAuth.js
+++ api/policies/UserSessionAuth.js
@@ -12,7 +12,6 @@ module.exports = function (req, res, next) {
 
   // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
-  var forbiddenmessage = 'You are not permitted to perform this action.';
   var token = req.headers["x-authorization"];
   if (token) {
     JWT.verifyToken(token, function (err, decoded) {
@@ -29,14 +28,14 @@ module.exports = function (req, res, next) {
           return next();
         } else {
           return res.forbidden({
-            message: 'This User Type not permitted to perform this action.'
+            message: constants.messages.ACCESS_FORBIDDEN
           })
         }
       }
     });
   } else {
     return res.forbidden({
-      message: forbiddenmessage
+      message: constants.messages.ACCESS_FORBIDDEN
     });
   }
 };
