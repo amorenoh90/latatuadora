3a738e09bf1efbe4b9522e36eea9578e66255b68
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-09-25 14:05:55 -0500

    #36 Added getAll action

--- api/services/ItineraryService.js
+++ api/services/ItineraryService.js
@@ -16,13 +16,40 @@ module.exports = {
       .create(values)
       .then(function () {
         result.messages.push(messages.ADDED_ITINERARY);
+
+        done(null, result);
       })
       .catch(function (err) {
         result.messages = [];
         result.errors.push(err);
+
+        done(err, result);
+      });
+  },
+
+  getAll: function (options, done) {
+    var values = options.input || {},
+      result = {
+        messages: [],
+        json_response: {},
+        errors: []
+      };
+
+    Itinerary
+      .find(values)
+      .populateAll()
+      .then(function (itineraries) {
+        if (itineraries.length < 1) result.messages.push(messages.NO_ITINERARIES);
+
+        result.json_response.itineraries = itineraries;
+
+        done(null, result);
       })
-      .then(function () {
-        done(result);
+      .catch(function (err) {
+        result.messages = [];
+        result.errors.push(err);
+
+        done(err, result);
       });
   }
 };
