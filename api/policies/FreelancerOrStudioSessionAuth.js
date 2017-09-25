cef5085515a9bc5c02b0c95a78f9a1a96b71c04c
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-09-25 14:04:23 -0500

    #36 Fixed typo

--- api/policies/FreelancerOrStudioSessionAuth.js
+++ api/policies/FreelancerOrStudioSessionAuth.js
@@ -1,6 +1,6 @@
 var constants = require('../Constants');
 module.exports = function (req, res, next) {
-  
+
   // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
   var forbiddenmessage = 'You are not permitted to perform this action.';
@@ -8,17 +8,23 @@ module.exports = function (req, res, next) {
   if (token) {
     JWT.verifyToken(token, function (err, decoded) {
       if (err) {
-        return res.forbidden({message: err.message});
+        return res.forbidden({
+          message: err.message
+        });
       } else {
         var user = {
           id: decoded.sub
         };
-        if (decoded.typ == constants.userType.freelancer) {
+        if (decoded.typ == constants.userType.freelance) {
           req.headers.user = user;
-          Freelancer.findOne({userId: user.id})
+          Freelancer.findOne({
+              user: user.id
+            })
             .then(function (freelancer) {
               if (!freelancer) {
-                return res.notFound({message: 'Could not find Freelancer, sorry.'});
+                return res.notFound({
+                  message: 'Could not find Freelancer, sorry.'
+                });
               } else {
                 req.headers.freelancer = freelancer;
                 if (req.body) {
@@ -33,10 +39,14 @@ module.exports = function (req, res, next) {
         }
         if (decoded.typ == constants.userType.studio) {
           req.headers.user = user;
-          Studio.findOne({userId: user.id})
+          Studio.findOne({
+              userId: user.id
+            })
             .then(function (studio) {
               if (!studio) {
-                return res.notFound({message: 'Could not find Studio, sorry.'});
+                return res.notFound({
+                  message: 'Could not find Studio, sorry.'
+                });
               } else {
                 req.headers.studio = studio;
                 if (req.body) {
@@ -49,11 +59,15 @@ module.exports = function (req, res, next) {
               return res.serverError(err);
             });
         } else {
-          return res.forbidden({message: 'This User Type not permitted to perform this action.'})
+          return res.forbidden({
+            message: 'This User Type not permitted to perform this action.'
+          })
         }
       }
     });
   } else {
-    return res.forbidden({message: forbiddenmessage});
+    return res.forbidden({
+      message: forbiddenmessage
+    });
   }
 };
