534549bedef20fcc2df275690093ad344e690584
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-09-27 11:08:55 -0500

    #44 - Added getNearby action

--- api/services/StudioService.js
+++ api/services/StudioService.js
@@ -1,5 +1,27 @@
 var constants = require('../Constants.js'),
   messages = constants.messages;
+
+const radius = 0.5;
+
+function calculateDistance(x0, x, y0, y) {
+  var distance = Math.pow(x - x0, 2) + Math.pow(y - y0, 2);
+  distance = Math.sqrt(distance);
+
+  return distance;
+}
+
+function filterAddresses(addresses, x0, y0) {
+  var filtered_addresses = [];
+  for (var i = 0; i < addresses.length; i++) {
+    var address = addresses[i],
+      distance = calculateDistance(x0, address.long, y0, address.lat);
+
+    if (distance <= radius) filtered_addresses.push(address.id);
+  }
+
+  return filtered_addresses;
+}
+
 module.exports = {
   getAll: function (options, done) {
     var values = options.input || {},
@@ -34,5 +56,45 @@ module.exports = {
 
         done(err, result);
       });
-  }
+  },
+
+  getNearby: function (options, done) {
+    var input = options.input || {},
+      result = {
+        messages: [],
+        json_response: {},
+        errors: []
+      },
+      long = input.long || 0,
+      lat = input.lat || 0;
+
+    Address
+      .find()
+      .sort('createdAt ASC')
+      .then(function (addresses) {
+        if (addresses.length < 1) {
+
+          done(null, messages.NO_USERS_UNDER_CRITERIA);
+        } else {
+          addresses = filterAddresses(addresses, long, lat);
+
+          Studio
+            .find({
+              addressId: addresses
+            })
+            .sort('createdAt ASC')
+            .then(function (studios) {
+              result.json_response.studios = studios;
+
+              done(null, result);
+            })
+            .catch(function (err) {
+              done(err, null);
+            });
+        }
+      })
+      .catch(function (err) {
+        done(err, null);
+      });
+  },
 };
