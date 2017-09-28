1ea622294d477ea57d86d01ea084b9818b903719
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-09-28 09:44:11 -0500

    #48 - Fixed null pointer

--- api/services/TemplateService.js
+++ api/services/TemplateService.js
@@ -4,7 +4,10 @@ var fs = require('fs');
 var retrieveContent = function (templateName, model) {
   var templateLocation = "views/mail/" + templateName;
   var fileContent = fs.readFileSync(templateLocation, "utf8");
-  return ejs.render(fileContent, model, {filename: templateLocation});
+  return ejs.render(fileContent, {
+    filename: templateLocation,
+    model: model
+  });
 };
 
 module.exports = {
