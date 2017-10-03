f0026405b20fbde4712937077e033918a1cc336e
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-10-03 11:41:12 -0500

    #61 - Created Lead model - rash240719

--- api/models/Lead.js
+++ api/models/Lead.js
@@ -1,56 +1,12 @@
-/**
- * Quotation.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- */
-
 module.exports = {
-  
+
   attributes: {
-    dimensionsX: {
-      type: 'float',
-      size: 5,
-      required: true
+    quotation: {
+      model: "Quotation"
     },
-    dimensionsY: {
-      type: 'float',
-      size: 5,
-      required: true
-    },
-    styleId: {
-      model: 'Style'
-    },
-    comments: {
-      type: "string",
-      maxLength: 265
-    },
-    userId: {
+    jobber: {
       model: "User"
     },
-    freelancerId: {
-      model: "Freelancer"
-    },
-    references: {
-      collection: 'QuotationReferences',
-      via: 'quotation'
-    },
-    bodypartId: {
-      model: 'BodyPart'
-    },
-    studioId: {
-      model: 'Studio'
-    },
-    used: {
-      type: "boolean",
-      defaultsTo: false
-    },
-    id: {
-      autoIncrement: true,
-      type: 'integer',
-      unique: true,
-      primaryKey: true
-    }
   },
-  tableName: 'Quotation'
+  tableName: 'Lead'
 };
