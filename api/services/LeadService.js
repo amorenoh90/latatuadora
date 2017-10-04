59eb123f3da7cce32f58bb673508b3f7fce48c5e
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-10-04 16:57:07 -0500

    #63 - Added purchaseCredits action - rash240719

--- api/services/LeadService.js
+++ api/services/LeadService.js
@@ -1,85 +1,46 @@
-var send = function (values, done) {
-  // var api_key = 'key-5de07e0e2df132f47cfade8f2e52b89a';
-  // var domain = 'sandbox066fdbbdf16943069d9b0c3f92882717.mailgun.org';
-  var api_key = 'key-5de07e0e2df132f47cfade8f2e52b89a';
-  //var domain = 'https://api.mailgun.net/v3/mg.neopraxis.mx';
-  var domain = 'sandbox066fdbbdf16943069d9b0c3f92882717.mailgun.org';
-  var mailgun = require('mailgun-js')({
-    apiKey: api_key,
-    domain: domain
-  });
-  var contentHtml;
+function purchaseCredits(values, done) {
+  var input = values.input || {},
+    result = {
+      messages: [],
+      json_response: {},
+      errors: []
+    };
 
-  //var toRecipients = ['latatuadora@latatuadora.com', (values.to || '')].join(",");
-  //var toRecipients = ['alberto@blick.mx', (values.to || '')].join(",");
-  var toRecipients = ['dyll240719@gmail.com', (values.to || '')].join(",");
+  var doQuery = async (cb) => {
+      try {
+        var user = await
+          User
+            .find({
+              id: input.user
+            });
 
-  if (values.template) {
-    contentHtml = TemplateService.retrieveContent(values.template, values.model);
-  }
+        if (user.length < 1) {
+          result.messages.push(constants.messages.NO_SUCH_USER);
+        } else {
+          user = user[0];
+          user.lead_credits += constants.lead_amount[input.lead_amount];
 
-  var data = {
-    from: 'latatuadora@latatuadora.com',
-    to: toRecipients,
-    subject: values.subject,
-    html: contentHtml ? contentHtml : values.text
-  };
-  mailgun.messages().send(data, function (err, body) {
-    if (done) {
-      if (err) {
-        done(err, null)
-      } else {
-        var message = {
-          "message": "email sended"
-        };
-        done(null, message);
+          user = await User
+            .update({
+              id: user.id
+            }, {
+              lead_credits: user.lead_credits
+            });
+
+          result.json_response.user = user;
+        }
+      } catch (error) {
+        result.messages = [];
+        result.errors.push(error);
+      } finally {
+        cb(result.errors.pop(), result);
       }
     }
-  });
-};
+  ;
+
+  doQuery(done);
+}
 
 module.exports = {
-  send: function (values, done) {
-    values.template = 'template.ejs';
-    if (!values.model) values.model = "";
-    return send(values, done);
-  },
-  sendUserWelcomeMail: function (values, done) {
-    values.template = 'welcome-user.ejs';
-    values.subject = "Bienvenido a la tatuadora";
-    return send(values, done);
-  },
-  sendStudioWelcomeMail: function (values, done) {
-    values.template = 'welcome-studio-freelance.ejs';
-    return send(values, done);
-  },
-  sendAdminWelcomeMail: function (values, done) {
-    values.template = 'welcome-admin.ejs';
-    return send(values, done);
-  },
-  sendStudioQuotation: function (values, done) {
-    values.template = 'quotation-studio-freelance.ejs';
-    values.subject = "COTIZACION #" + values.model.quotation.id;
-    return send(values, done);
-  },
-  sendAdminQuotation: function (values, done) {
-    values.template = 'quotation-latatuadora.ejs';
-    values.subject = "COTIZACION #" + values.model.quotation.id;
-    if (values.model.quotation.minAmount && values.model.quotation.maxAmount) {
-      values.subject = values.subject + " RANGO DE PRECIO: " + values.model.quotation.minAmount + " - " + values.model.quotation.maxAmount;
-    }
-    return send(values, done);
-  },
-  sendUserQuotation: function (values, done) {
-    values.template = 'quotation-user.ejs';
-    if (values.model.quotation.studio) {
-      values.subject = "Tu cotización se ha enviado con éxito al estudio: " + values.model.quotation.studio.name;
-    } else if (values.model.quotation.freelancer) {
-      values.subject = "Tu cotización se ha enviado con éxito al tatuador independiente: " + values.model.quotation.freelancer.name;
-    } else {
-      values.subject = "Tu cotización se ha enviado con éxito";
-    }
-
-    return send(values, done);
-  }
+  purchaseCredits: purchaseCredits
 };
