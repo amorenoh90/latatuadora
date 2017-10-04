eda708af13f34b9dcb893869825c03c9ca0d6300
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-10-04 16:42:40 -0500

    #63 - Optimized async code - rash240719

--- api/services/AddressService.js
+++ api/services/AddressService.js
@@ -1,5 +1,42 @@
 module.exports = {
+  /*
+    add: function (values, done) {
+      var newAddress = {
+          street: values.street,
+          numInt: values.numInt,
+          numExt: values.numExt,
+          lat: values.lat,
+          long: values.long,
+          zc: values.zc
+        },
+        newtown = {
+          name: values.town
+        },
+        newsuburb = {
+          name: values.suburb
+        },
+        newstate = {
+          name: values.state
+        };
+      State.findOrCreate(newstate).then(function (state) {
+        newsuburb.stateId = state.id;
+        Suburb.findOrCreate(newsuburb).then(function (suburb) {
+          Town.findOrCreate(newtown).then(function (town) {
+            newAddress.stateId = state.id;
+            newAddress.suburbId = suburb.id;
+            newAddress.townId = town.id;
+            Address.create(newAddress).then(function (address) {
+              done(null, address.id);
+            });
+          });
+        });
+      }).catch(function (err) {
+        if (err) return done(err);
+      });
+    }
+    */
   add: function (values, done) {
+    var err;
     var newAddress = {
         street: values.street,
         numInt: values.numInt,
@@ -17,20 +54,31 @@ module.exports = {
       newstate = {
         name: values.state
       };
-    State.findOrCreate(newstate).then(function (state) {
-      newsuburb.stateId = state.id;
-      Suburb.findOrCreate(newsuburb).then(function (suburb) {
-        Town.findOrCreate(newtown).then(function (town) {
-          newAddress.stateId = state.id;
-          newAddress.suburbId = suburb.id;
-          newAddress.townId = town.id;
-          Address.create(newAddress).then(function (address) {
-            done(null, address.id);
-          });
-        });
-      });
-    }).catch(function (err) {
-      if (err) return done(err);
-    });
+
+    var doQuery = async () => {
+      try {
+        var state = await State.findOrCreate(newstate);
+
+        newsuburb.stateId = state.id;
+
+        var suburb = await Suburb.findOrCreate(newsuburb);
+        var town = await Town.findOrCreate(newtown);
+
+        newAddress.stateId = state.id;
+        newAddress.suburbId = suburb.id;
+        newAddress.townId = town.id;
+
+        var address = await Address.create(newAddress);
+      }
+      catch (e) {
+        console.log(e);
+        err = e;
+      }
+      finally {
+        done(err, address.id);
+      }
+    };
+
+    doQuery();
   }
 };
