2271352545796fa84bd66222aafabc580cf2e560
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-10-04 16:43:10 -0500

    #63 - Optimized async code - rash240719

--- api/services/AddressService.js
+++ api/services/AddressService.js
@@ -1,40 +1,4 @@
 module.exports = {
-  /*
-    add: function (values, done) {
-      var newAddress = {
-          street: values.street,
-          numInt: values.numInt,
-          numExt: values.numExt,
-          lat: values.lat,
-          long: values.long,
-          zc: values.zc
-        },
-        newtown = {
-          name: values.town
-        },
-        newsuburb = {
-          name: values.suburb
-        },
-        newstate = {
-          name: values.state
-        };
-      State.findOrCreate(newstate).then(function (state) {
-        newsuburb.stateId = state.id;
-        Suburb.findOrCreate(newsuburb).then(function (suburb) {
-          Town.findOrCreate(newtown).then(function (town) {
-            newAddress.stateId = state.id;
-            newAddress.suburbId = suburb.id;
-            newAddress.townId = town.id;
-            Address.create(newAddress).then(function (address) {
-              done(null, address.id);
-            });
-          });
-        });
-      }).catch(function (err) {
-        if (err) return done(err);
-      });
-    }
-    */
   add: function (values, done) {
     var err;
     var newAddress = {
