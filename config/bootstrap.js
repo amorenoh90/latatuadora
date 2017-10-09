2752af6cce7f85392b1eae1abad1286d84dbbfad
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-10-09 11:12:36 -0500

    #67 - Added bodyparts to bootstrap file

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,5 +1,44 @@
 global.constants = require('../api/Constants');
 
+const bodyparts = [
+  {
+    name: "Left Arm",
+    section: 0,
+    path: "a path"
+  },
+  {
+    name: "Right Arm",
+    section: 0,
+    path: "a path"
+  },
+  {
+    name: "Left leg",
+    section: 0,
+    path: "a path"
+  },
+  {
+    name: "Right leg",
+    section: 0,
+    path: "a path"
+  },
+  {
+    name: "Left hand",
+    section: 0,
+    path: "a path"
+  },
+  {
+    name: "Right hand",
+    section: 0,
+    path: "a path"
+  }];
+
 module.exports.bootstrap = function (cb) {
-  cb();
+
+  var doQuery = async () => {
+    await BodyPart.createEach(bodyparts);
+
+    cb();
+  };
+
+  doQuery();
 };
