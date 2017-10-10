e85e5de0a5c55d31edd342d307d380c8ea35b200
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-10-10 11:27:26 -0500

    #67 - Added bodyparts creation when needed

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -3,17 +3,56 @@
  */
 var sails = require('sails');
 
+const bodyparts = [
+  {
+    name: "Left Arm",
+    section: 0,
+    path: "a path"
+  },
+  {
+    name: "Right Arm",
+    section: 0,
+    path: "a path"
+  },
+  {
+    name: "Left leg",
+    section: 0,
+    path: "a path"
+  },
+  {
+    name: "Right leg",
+    section: 0,
+    path: "a path"
+  },
+  {
+    name: "Left hand",
+    section: 0,
+    path: "a path"
+  },
+  {
+    name: "Right hand",
+    section: 0,
+    path: "a path"
+  }];
+
 before(function (done) {
-  
+
   // Increase the Mocha timeout so that Sails has enough time to lift.
   this.timeout(100000);
-  
+
   sails.lift({
     // configuration for testing purposes
   }, function (err) {
     if (err) return done(err);
-    // here you can load fixtures, etc.
-    done(err, sails);
+
+    var doQuery = async () => {
+      var bodyparts_before_adding = await BodyPart.find().populateAll();
+      if (bodyparts_before_adding.length < 6) await BodyPart.createEach(bodyparts);
+
+      done(err, sails);
+    };
+
+    doQuery();
   });
 });
 
