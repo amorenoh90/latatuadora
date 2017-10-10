aabbfc9b24b10cc389f5d1d576b83b475669cc96
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-10-10 13:17:08 -0500

    #76 - Added getAll action

--- api/controllers/ElementController.js
+++ api/controllers/ElementController.js
@@ -1,15 +1,15 @@
 const constants = require('../Constants');
 module.exports = {
-  send: function (req, res) {
-    EmailService
-      .send(req.body, function (error, result) {
+  getAll: function (req, res) {
+    ElementService
+      .getAll({}, function (error, result) {
         if (error) {
           res.serverError({
             error: error
           });
         } else {
           res.send({
-            message: constants.messages.EMAIL_SENT
+            elements: result
           });
         }
       });
