4dc2f1b4b904000715f9cd6dae782fdd40cc62ae
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-10-11 12:30:43 -0500

    #84 - Added get action

--- api/services/TattooService.js
+++ api/services/TattooService.js
@@ -1,87 +1,34 @@
-var send = function (values, done) {
-  // var api_key = 'key-5de07e0e2df132f47cfade8f2e52b89a';
-  // var domain = 'sandbox066fdbbdf16943069d9b0c3f92882717.mailgun.org';
-  //var api_key = 'key-5de07e0e2df132f47cfade8f2e52b89a';
-  var api_key = 'key-eb656047b090ea091ef7c5d2fbd83dc5';
-  //var domain = 'https://api.mailgun.net/v3/mg.neopraxis.mx';
-  //var domain = 'sandbox066fdbbdf16943069d9b0c3f92882717.mailgun.org';
-  var domain = 'https://api.mailgun.net/v3/sandbox3bfa1334fbee4dcca5b08a9b34b46337.mailgun.org';
-  var mailgun = require('mailgun-js')({
-    apiKey: api_key,
-    domain: domain
-  });
-  var contentHtml;
-
-  //var toRecipients = ['latatuadora@latatuadora.com', (values.to || '')].join(",");
-  //var toRecipients = ['alberto@blick.mx', (values.to || '')].join(",");
-  var toRecipients = ['dyll240719@gmail.com', (values.to || '')].join(",");
-
-  if (values.template) {
-    contentHtml = TemplateService.retrieveContent(values.template, values.model);
-  }
-
-  var data = {
-    from: 'latatuadora@latatuadora.com',
-    to: toRecipients,
-    subject: values.subject,
-    html: contentHtml ? contentHtml : values.text
-  };
-  mailgun.messages().send(data, function (err, body) {
-    if (done) {
-      if (err) {
-        done(err, null)
-      } else {
-        var message = {
-          "message": "email sended"
-        };
-        done(null, message);
-      }
-    }
-  });
-};
-
 module.exports = {
-  send: function (values, done) {
-    values.template = 'template.ejs';
-    if (!values.model) values.model = "";
-    return send(values, done);
-  },
-  sendUserWelcomeMail: function (values, done) {
-    values.template = 'welcome-user.ejs';
-    values.subject = "Bienvenido a la tatuadora";
-    return send(values, done);
-  },
-  sendStudioWelcomeMail: function (values, done) {
-    values.template = 'welcome-studio-freelance.ejs';
-    return send(values, done);
-  },
-  sendAdminWelcomeMail: function (values, done) {
-    values.template = 'welcome-admin.ejs';
-    return send(values, done);
-  },
-  sendStudioQuotation: function (values, done) {
-    values.template = 'quotation-studio-freelance.ejs';
-    values.subject = "COTIZACION #" + values.model.quotation.id;
-    return send(values, done);
-  },
-  sendAdminQuotation: function (values, done) {
-    values.template = 'quotation-latatuadora.ejs';
-    values.subject = "COTIZACION #" + values.model.quotation.id;
-    if (values.model.quotation.minAmount && values.model.quotation.maxAmount) {
-      values.subject = values.subject + " RANGO DE PRECIO: " + values.model.quotation.minAmount + " - " + values.model.quotation.maxAmount;
-    }
-    return send(values, done);
-  },
-  sendUserQuotation: function (values, done) {
-    values.template = 'quotation-user.ejs';
-    if (values.model.quotation.studio) {
-      values.subject = "Tu cotización se ha enviado con éxito al estudio: " + values.model.quotation.studio.name;
-    } else if (values.model.quotation.freelancer) {
-      values.subject = "Tu cotización se ha enviado con éxito al tatuador independiente: " + values.model.quotation.freelancer.name;
-    } else {
-      values.subject = "Tu cotización se ha enviado con éxito";
-    }
+  get: function (values, done) {
+    var input = values.input || {},
+      result = {
+        messages: [],
+        json_response: {},
+        errors: []
+      };
 
-    return send(values, done);
-  }
+    var doQuery = async (cb) => {
+      try {
+        var tattoo = await Tattoo
+          .find({
+            id: input.tattoo
+          })
+          .sort('id ASC')
+          .populateAll();
+
+        if (tattoo.length < 1) result.messages.push(messages.NO_TATTOOS_UNDER_CRITERIA);
+
+        result.json_response.tattoo = tattoo[0];
+      } catch
+        (error) {
+        result.messages = [];
+        result.errors.push(error);
+      }
+      finally {
+        cb(result.errors.pop(), result);
+      }
+    };
+
+    doQuery(done);
+  },
 };
