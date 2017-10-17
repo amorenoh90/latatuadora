49705e8b4d7fd6656d6be1c38f89ba9b27021a17
Rashīd Teoyokonetl <rash240719@gmail.com>
2017-10-17 12:20:56 -0500

    #98 - Added user getById action

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -55,5 +55,61 @@ module.exports = {
     };
 
     doQuery();
+  },
+
+  getById: function (options, done) {
+    var values = options.input || {};
+
+    var doQuery = async () => {
+      var user;
+      try {
+        user = (await User
+          .find({
+            id: values.user
+          })
+          .populateAll())[0],
+          errors = null;
+
+        if (!user) {
+          user = messages.NO_SUCH_USER;
+        } else {
+          user.studio = (await Studio
+            .find({
+              userId: user.id
+            })
+            .populateAll())[0] || null,
+            user.freelancer = (await Freelancer
+              .find({
+                user: user.id
+              })
+              .populateAll())[0] || null;
+
+          if (user.studio || user.freelancer) {
+            if (!user.studio) user.studio = {addressId: {id: 0}};
+            if (!user.studio.addressId) {
+              user.studio.addressId = {id: 0};
+            }
+
+            if (!user.freelancer) user.freelancer = {addressId: {id: 0}};
+            if (!user.freelancer.addressId) {
+              user.freelancer.addressId = {id: 0};
+            }
+
+            user.addressId = user.studio.addressId.id || user.freelancer.addressId.id;
+            user.addressId = (await Address
+              .find({
+                id: user.addressId
+              })
+              .populateAll())[0] || null;
+          }
+        }
+      } catch (error) {
+        errors = error;
+      } finally {
+        done(errors, user);
+      }
+    };
+
+    doQuery();
   }
 };
